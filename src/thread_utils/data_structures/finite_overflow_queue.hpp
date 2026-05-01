864cac2286aef2f5cf352ee1d079153bde2f45d3
drfailer <drfailer.42@gmail.com>
2026-05-01 16:19:10 -0400

    fix: the overflow flag doesn't work

--- src/thread_utils/data_structures/finite_overflow_queue.hpp
+++ src/thread_utils/data_structures/finite_overflow_queue.hpp
@@ -15,7 +15,6 @@ template <typename T, size_t SIZE = 1024>
 struct TU_FiniteOverflowQueue {
     TU_LockFreeQueue<T> overflow_queue;
     TU_FiniteLockFreeQueue<T, SIZE> finite_queue;
-    TU_AtomicFlag overflow_flag = false;
     void push(T value);
     bool pop(T *result);
 
@@ -23,31 +22,64 @@ struct TU_FiniteOverflowQueue {
     TU_FiniteOverflowQueue(TU_FiniteOverflowQueue const &) = delete;
     TU_FiniteOverflowQueue(TU_FiniteOverflowQueue &&other)
         : overflow_queue(std::move(other.overflow_queue)),
-          finite_queue(std::move(other.finite_queue)),
-          overflow_flag(other.overflow_flag.load()) {}
+          finite_queue(std::move(other.finite_queue)) {}
 };
 
 template <typename T, size_t SIZE>
 void TU_FiniteOverflowQueue<T, SIZE>::push(T value) {
-    // we keep trying to add to the finite queue in case the flag is note up to date
     if (this->finite_queue.push(value)) [[likely]] {
         return;
     }
-    // overflow, update the flag
-    this->overflow_flag.store(true, std::memory_order_release);
     this->overflow_queue.push(value);
 }
 
 template <typename T, size_t SIZE>
 bool TU_FiniteOverflowQueue<T, SIZE>::pop(T *result) {
-    if (this->overflow_flag.load(std::memory_order_acquire)) [[unlikely]] {
-        if (this->overflow_queue.pop(result)) {
-            return true;
-        }
-        // the overflow queue is empty so we change the flag
-        this->overflow_flag.store(false, std::memory_order_release);
+    if (this->finite_queue.pop(result)) [[likely]] {
+        return true;
     }
-    return this->finite_queue.pop(result);
+    return this->overflow_queue.pop(result);
 }
+
+
+// BUG: I tried a version with a flag but it doesn't work
+// template <typename T, size_t SIZE = 1024>
+// struct TU_FiniteOverflowQueue {
+//     TU_LockFreeQueue<T> overflow_queue;
+//     TU_FiniteLockFreeQueue<T, SIZE> finite_queue;
+//     alignas(CACHE_LINE) TU_AtomicFlag overflow_flag = false;
+//     void push(T value);
+//     bool pop(T *result);
+//
+//     TU_FiniteOverflowQueue() = default;
+//     TU_FiniteOverflowQueue(TU_FiniteOverflowQueue const &) = delete;
+//     TU_FiniteOverflowQueue(TU_FiniteOverflowQueue &&other)
+//         : overflow_queue(std::move(other.overflow_queue)),
+//           finite_queue(std::move(other.finite_queue)),
+//           overflow_flag(other.overflow_flag.load()) {}
+// };
+//
+// template <typename T, size_t SIZE>
+// void TU_FiniteOverflowQueue<T, SIZE>::push(T value) {
+//     // we keep trying to add to the finite queue in case the flag is note up to date
+//     if (this->finite_queue.push(value)) [[likely]] {
+//         return;
+//     }
+//     // overflow, update the flag
+//     this->overflow_flag.store(true, std::memory_order_release);
+//     this->overflow_queue.push(value);
+// }
+//
+// template <typename T, size_t SIZE>
+// bool TU_FiniteOverflowQueue<T, SIZE>::pop(T *result) {
+//     if (this->overflow_flag.load(std::memory_order_acquire)) [[unlikely]] {
+//         if (this->overflow_queue.pop(result)) {
+//             return true;
+//         }
+//         // the overflow queue is empty so we change the flag
+//         this->overflow_flag.store(false, std::memory_order_release);
+//     }
+//     return this->finite_queue.pop(result);
+// }
 
 #endif
